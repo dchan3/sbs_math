6e3fbaaac3d599ef214cb8410e9fceab3250b09a
lbclofy <lofy@hawaii.edu>
2016-01-19 07:53:59 -1000

    Update Animal.lua
    font.

--- Animal.lua
+++ Animal.lua
@@ -4,7 +4,7 @@ function Animal:new(file, w, h, fontsize)
 	local animal = display.newGroup()
 	animal.name = "animal"
 	animal.ball = display.newImageRect(file, w, h)
-	animal.text = display.newText("", 0, 0, native.systemFont, fontsize)
+	animal.text = display.newText("", 0, 0, font, fontsize)
 	animal.num = 0
 	animal.matched = false
 	return animal
