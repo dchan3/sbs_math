54de98bd160a7d1c034a9afee34beef48bf1e057
lbclofy <lofy@hawaii.edu>
2016-01-19 07:19:09 -1000

    Update AnimalBall.lua
    Added ball parameter

--- AnimalBall.lua
+++ AnimalBall.lua
@@ -2,6 +2,7 @@ AnimalBall = {}
 
 function AnimalBall:new(x,y,r,i) 
 	local ball = display.newGroup()
+	ball.name = "number"
 	ball.ball = display.newCircle( 0, 0, r )
 	ball.ball:setFillColor(Blue.R, Blue.G, Blue.B)
 	ball.text = display.newText(i, 0, 0, native.systemFont, ballR*.8 )
