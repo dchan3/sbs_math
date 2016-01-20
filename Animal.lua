e73dcd69d13c1d73b99f3a6be1f522750d45fd5e
lbclofy <lofy@hawaii.edu>
2016-01-20 10:58:03 -1000

    Update Animal.lua
    Updated to allow for highlight

--- Animal.lua
+++ Animal.lua
@@ -4,8 +4,12 @@ function Animal:new(file, w, h, fontsize)
 	local animal = display.newGroup()
 	animal.name = "animal"
 	animal.ball = display.newImageRect(file, w, h)
+	animal.outline = display.newImageRect("highlight.png", w, h)
+	animal.outline:setFillColor(0,0,0)
+	animal.outline.alpha = 0
 	animal.text = display.newText("", 0, 0, font, fontsize)
 	animal.num = 0
 	animal.matched = false
+	animal:insert( animal.outline )
 	return animal
 end
