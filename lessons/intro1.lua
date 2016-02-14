dc1d18e008017aa7ceb40ec4e466db998e8f2a60
Chad Delp <cdelp@hawaii.edu>
2016-02-14 13:40:39 -1000

    lesson 1 intro animation added menu changed to allow access to animation

--- lessons/intro1.lua
+++ lessons/intro1.lua
@@ -1,20 +1,24 @@
+-- intro1.lua
+
+--[[ 
+    Animated tutorial scene for lesson one. Opens lesson 1 upon completion.
+    Set timing of events in timers at the end of scene:create.
+    Set size, position, and transition of objects in tutorial functions below 
+    variable list. 
+]]--
+
 local composer = require( "composer" )
 local numLine = require( "objects.numLine" )
 local animal = require("objects.animal")
 local animalball = require("objects.animalball")
 local physics = require "physics"
 physics.start()
---physics.setDrawMode( "hybrid" )
 
 
 local scene = composer.newScene()
 
-local hasCollidedCircle
-
 local max = 3
-local count = max -- math.random( 1, max)
---local matchCount = count
-local map = { false, false, false, false, false, false, false, false, false, false,  false, false, false, false, false }
+local count = max
 
 local countBalls = {}
 local matchBalls = {}
@@ -26,18 +30,15 @@ local sceneGroup
 local decText
 local latText
 
-local selText = display
-
 -- variables and functions for tutorial -------------------------------------
 
 local boardText
 local hand
 local board
-local tryText
 local moveBall
 local animal
 
--- displays chalk board
+-- fades chalkboard into scene
 local function showBoard ( event )
     board = display.newImage( "images/small_chalkboard.png" )
     board.x = _W * .7
@@ -47,6 +48,7 @@ local function showBoard ( event )
     transition.fadeIn( board, {time=500} )   
 end
 
+-- fades hand into scene
 local function showHand ( event )
     hand = display.newImage( "images/hand.png" )
     hand.x = _W * .6
@@ -55,6 +57,7 @@ local function showHand ( event )
     transition.fadeIn( hand, {time=500} )
 end
 
+-- chalkboard text sequence
 local function textDisplay ( event )
     boardText = display.newText( "Drag the numbers", _W*.7, 
             _H*.25, font, 40 )   
@@ -69,26 +72,24 @@ local function text3 ( event )
 end
 
 local function text4 ( event )
-    boardText.text = "now YOU try!"
+    boardText.text = "Now YOU try!"
     boardText:setFillColor(1,0,0)
 end
 
+-- upward hand transition
 local function handMoveUp( event )
     transition.to(hand, {x= _W * .25, y=_H -380, time=2000 })
 end
 
+-- downward hand and ball transitions
 local function handMoveDown ( event )
     transition.to(hand, {x= _W *.4, y= _H*.8, time=1500 })
     transition.to(moveBall, {x= _W *.3, y= _H*.7, time=1500 })
 end
 
+-- fades animal when covered by number. set timing below
 local function animalFade ( event )
     animal.alpha = .3
-end
-
-local function youTry ( event )
-    tryText = display.newText( "Now you try!", _W*.5, _H*.6, font, 80)
-    tryText:setFillColor(1,0,0)
 end
 
 local function endIntro()
@@ -104,185 +105,6 @@ local function endIntro()
 end
 -- end tutorial functions
 ---------------------------------------------------------------
---[[
-local function drag( event )
-    local t = event.target
-
-    -- Print info about the event. For actual production code, you should
-    -- not call this function because it wastes CPU resources.
-    --printTouch(event)
-
-    local phase = event.phase
-    if "began" == phase then
-        -- Make target the top-most object
-        local parent = t.parent
-        parent:insert( t )
-        display.getCurrentStage():setFocus( t )
-
-        decText.text = t.num
-        local text = convertDecToLat( t.num )
-        latText.text = text
-        -- Spurious events can be sent to the target, e.g. the user presses
-        -- elsewhere on the screen and then moves the finger over the target.
-        -- To prevent this, we add this flag. Only when it's true will "move"
-        -- events be sent to the target.
-        t.isFocus = true
-
-        -- Store initial position
-        t.x0 = event.x - t.x
-        t.y0 = event.y - t.y
-    elseif t.isFocus then
-        if "moved" == phase then
-            -- Make object move (we subtract t.x0,t.y0 so that moves are
-            -- relative to initial grab point, rather than object "snapping").
-            t.x = event.x - t.x0
-            t.y = event.y - t.y0
-
-        elseif "ended" == phase or "cancelled" == phase then
-
-                print(t.cooldedWith)
-
-                if(t.collidedWith ~= nil) then
-
-                    hasCollidedCircle(t, t.collidedWith )
-
-                end
-
-            display.getCurrentStage():setFocus( nil )
-            t.isFocus = false
-        end
-    end
-
-    -- Important to return true. This tells the system that the event
-    -- should not be propagated to listeners of any objects underneath.
-    return true
-end 
-
-local function onLocalCollision( self, event )
-   local t = event.target
-   local o = event.other
-
-    if ( event.phase == "began" ) then
-
-        if (event.other.matched == false) then
-            event.other.num = self.num
-            self.collidedWith = event.other
-
-        end
-
-    elseif ( event.phase == "ended" ) or ( event.phase == "cancelled" ) then
-
-        if (event.other.matched == false) then
-            event.other.num = self.num
-            self.collidedWith = event.other
-        end
-
-    end
-end
-
-
-function hasCollidedCircle(obj1, obj2)
-
-    if obj1 == nil then
-        return false
-    end
-
-    if obj2 == nil then
-        return false
-    end
-
-    local sqrt = math.sqrt
-    local dx =  obj1.x - obj2.x;
-    local dy =  obj1.y - obj2.y;
-    local distance = sqrt(dx*dx + dy*dy);
-    local objectSize = (obj2.contentWidth/2) + (obj1.contentWidth/2)
-
-    if distance < objectSize then
-
-                    local function listener( event )
-                        display.remove(obj1)
-                    end
-
-
-                    local transitionTime = 100
-
-
-                    transition.to(obj1,
-                    {
-                        time=transitionTime,
-                        x = obj2.x,
-                        y= obj2.y,
-                        onComplete = listener
-                    })
-
-                   obj2.text = display.newText( obj1.num, 0, 0, font, ballR*2 )
-                    obj2.text:setFillColor(0,0,0)
-                    obj2.text.alpha = 0
-                    obj2.num = obj1.num
-                    obj2:insert( obj2.text )
-                    obj2.matched = true
-
-
-                    local function listener( event )
-                        transition.to(obj2.text,
-                        {
-                        time=500,
-                        alpha =.75
-                        })
-                    end
-                    timer.performWithDelay( transitionTime, listener )
-
-                    matchCount = matchCount - 1
-
-                    if matchCount <= 0 then
-											decText.text = ""
-											latText.text = ""
-                      check()
-											matchCount = count
-                    end
-        print("true")
-        return true
-    end
-    return false
-end
-]]--
---[[
-function reset()
-	displayText.text = ""
-	clearBalls()
-	count = math.random(1,max)
-    	matchCount = count
-  	initBalls()
-end ]]--
---[[
-function check()
-
-	for i=1, count do
-			local distance = math.pow( (math.pow( matchBalls[i].x - numberLine.x, 2 ) + math.pow( matchBalls[i].y, 2 )), .5  )
-			local time = distance/maxSpeed*1000
-			--print( numberLine.num[i].text .. " : ".. distance .. " : ".. time)
-			transition.moveTo( matchBalls[i], {x = numberLine.hash[matchBalls[i].num].x + numberLine.x, y = numberLine.hash[matchBalls[i].num].y + numberLine.y, time=1000})
-			--transition.matTrans( matchBalls[i], numberLine.hash[matchBalls[i].num].x + numberLine.x, numberLine.hash[matchBalls[i].num].y + numberLine.y, time )
-	end
-
-	for j=1,count do
-		timer.performWithDelay(j * 1000, function (event) displayText.text = convertDecToLat(j) end)
-
-        for i = 1, count do
-            if matchBalls[i].num == j then
-                timer.performWithDelay(j * 1000, 
-                    function (event)  
-                    matchBalls[i].outline:setFillColor(hlColor.R, hlColor.G, hlColor.B)
-                end)
-            end
-        end
-	end
-
-	local currScene = composer.getSceneName( "current" )
-	print(currScene)
-	timer.performWithDelay((count + 1) * 1000, function (event) reset() end)
-
-end ]]--
 
 function initBalls()
 	    for i=1,count do
@@ -362,7 +184,7 @@ function scene:create( event )
  		initBalls()
                 
     --------------------------------------
-    -- tutorial display objects, controled timing
+    -- timed function calls for tutorial objects and transitions
     
     -- delayed calls to display board and hand
     timer.performWithDelay( 500, showBoard)
